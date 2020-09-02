42ecd1611660d6e2e4807148c1a611a0d91f9fb1
himani88jain <himani7.88jain@gmail.com>
2020-09-02 11:51:47 -0400

    Initial commit

---
+++ src/test/java/co/grandcircus/MoviesCapstone/MoviesCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grandcircus.MoviesCapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
